df0b6d92c5e026223b35ac553ce2300557b7c74d
suprabha-girish <suprabhagirish323@gmail.com>
2024-10-17 12:54:46 +0530

    Add files via upload

---
+++ qrcodegener.py
@@ -0,0 +1,13 @@
+import qrcode
+import image
+qr = qrcode.QRCode(
+    version = 15,
+    box_size=10,
+    border=5
+)
+
+data = "suprabha"
+qr.add_data(data)
+qr.make (fit = True)
+img = qr.make_image(fill="black",black_color= "white")
+img.save("test.png")
